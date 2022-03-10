64bc1d59436e453941500cfce517eeb3f55f92b8
d3enis <79760898+d3enis@users.noreply.github.com>
2022-03-10 19:03:17 +0100

    ee

--- models/db.js
+++ models/db.js
@@ -2,19 +2,20 @@
 const mysql = require("mysql");
 
 //Spajanje na bazu
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 15,
   host: "eu-cdbr-west-02.cleardb.net",
   user: "b60652ce281183",
   password: "2d792b40",
   database: "heroku_b3b23fbf54bb51c",
 });
 
-db.connect(function (err) {
+/* db.connect(function (err) {
   if (err) {
     return console.error("error: " + err.message);
   }
 
   console.log("Spojeno na MySql bazu.");
 });
-
+ */
 module.exports = db;
