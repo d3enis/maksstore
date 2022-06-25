7afdd935d4a1f74fae2902f84d8dd27dd0476e5f
d3enis <79760898+d3enis@users.noreply.github.com>
2022-06-25 11:26:16 +0200

    env variables

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 //Importi
 const mysql = require("mysql");
+require("dotenv").config();
 
 //Spajanje na bazu
 const db = mysql.createConnection({
-  host: "eu-cdbr-west-02.cleardb.net",
-  user: "b60652ce281183",
-  password: "2d792b40",
-  database: "heroku_b3b23fbf54bb51c",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 /* db.connect(function (err) {
